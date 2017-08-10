9849d07fade5b7d60fc7fbbe330637a15e64dbd8
Tin Nguyen <trungtinnguyen1994@gmail.com>
2017-08-10 23:44:58 +0700

    Update main.js
    Remove console.log

--- main.js
+++ main.js
@@ -4,16 +4,13 @@
     var hasClassTransparent = false;
 
     var scrollHandle = function() {
-        console.log('dasdsadsa');
         var scrollTop = 0;
         if (navbar) {
             hasClassTransparent = navbar.className.indexOf('transparent') > -1;
 			scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-            console.log('sdas', scrollTop);
             if (scrollTop >= 10) {
 				navbar.classList.remove('transparent');
 			} else {
-                console.log('tran', hasClassTransparent, navbar.className);
 				if (!hasClassTransparent) {
 					navbar.classList.add('transparent');
 				}
